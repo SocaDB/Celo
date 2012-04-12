fb0b4f51687265543cd8bc4d7178ab0401a1dc29
Hugo LECLERC <leclerc@lmt.ens-cachan.fr>
2012-04-12 16:36:40 +0200

    url dec enc

--- src/Celo/StringHelp.cpp
+++ src/Celo/StringHelp.cpp
@@ -1,13 +1,60 @@
 #include "StringHelp.h"
 
+/*
+import string
+s = ""
+for i in range( 256 ):
+    c = chr( i )
+    if c.isalnum() or c == '-' or c == '_' or c == '.' or c == '~':
+        s += c
+    else:
+        s += "%"
+
+print 'static char conv[] = "' + s + '";'
+
+# from hex
+import string
+s = []
+for i in range( 256 ):
+    c = chr( i )
+    if c >= '0' and c <= '9':
+        s.append( str( i - ord( '0' ) ) )
+    elif c >= 'a' and c <= 'f':
+        s.append( str( 10 + i - ord( 'a' ) ) )
+    elif c >= 'A' and c <= 'F':
+        s.append( str( 10 + i - ord( 'A' ) ) )
+    else:
+        s.append( '0' )
+
+print 'static char from_hex[] = { ' + string.join( s, ',' ) + ' };'
+
+*/
+
 std::string url_encode( const std::string &str ) {
-    std::string res;
-    res.reserve( str.size() );
+    static char conv[] = "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%-.%0123456789%%%%%%%ABCDEFGHIJKLMNOPQRSTUVWXYZ%%%%_%abcdefghijklmnopqrstuvwxyz%%%~%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%";
+    static char to_hex[] = "0123456789ABCDEF";
+    std::string res; res.reserve( str.size() );
     for( int i = 0; i < str.size(); ++i ) {
-        if ( str[ i ] == ' ' )
-            res += "%20";
-        else
-            res += str[ i ];
+        unsigned char u = str[ i ];
+        char c = conv[ u ];
+        if ( c == '%' ) {
+            res += '%'; res += to_hex[ u >> 4 ]; res += to_hex[ u & 15 ];
+        } else
+            res += c;
     }
     return res;
 }
+
+std::string url_decode( const std::string &str ) {
+    static char from_hex[] = { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,2,3,4,5,6,7,8,9,0,0,0,0,0,0,0,10,11,12,13,14,15,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,10,11,12,13,14,15,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 };
+    std::string res;
+    for( int i = 0; i < str.size(); ++i ) {
+        if ( str[ i ] == '%' and i + 2 < str.size() )
+            res += from_hex[ str[ ++i ] ] << 4 | from_hex[ str[ ++i ] ];
+        else
+            res += str[ i ];
+    }
+
+    return res;
+}
+
--- src/Celo/StringHelp.h
+++ src/Celo/StringHelp.h
@@ -10,6 +10,7 @@
 
 inline bool is_a_number( char a ) { return a >= '0' and a <= '9'; }
 std::string url_encode( const std::string &str );
+std::string url_decode( const std::string &str );
 
 
 #endif // STRINGHELP_H
