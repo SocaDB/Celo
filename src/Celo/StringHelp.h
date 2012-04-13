b116680e635b9bf0c3bb10d7e97a0657265ee9bf
Hugo LECLERC <leclerc@lmt.ens-cachan.fr>
2012-04-13 13:57:13 +0200

    TODO

--- src/Celo/StringHelp.h
+++ src/Celo/StringHelp.h
@@ -7,6 +7,8 @@
     std::cout << "  " << #A << " -> " << (A) << std::endl
 #define PRINTN( A ) \
     std::cout << "  " << #A << " ->\n" << (A) << std::endl
+#define TODO \
+    std::cerr << "TODO " << __FILE__ << ":" << __LINE__ << std::endl
 
 inline bool is_a_number( char a ) { return a >= '0' and a <= '9'; }
 std::string url_encode( const std::string &str );
